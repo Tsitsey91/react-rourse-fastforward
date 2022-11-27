c8fc799dfb25d8066300a4d0cddddcf6734c6ecd
Chris_Vlachos <cv@mcinnovations.com.gr>
2022-11-27 21:43:42 +0200

    More Component Work & Styling with CSS classes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
+//React Components = functions returning JSX, which we use to 
+// tell React(and eventually the browser) what should appear on the
+//screen
 
 function App() {
   return (
-    <div>Hello!</div>
+    //JSX code 
+    <div>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
+//we export it, to make it available outside of this file
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App /> );
-
+//equivalent- but not working (27/11/2022)
+// ReactDOM.render(<App />, document.getElementById('root'))
