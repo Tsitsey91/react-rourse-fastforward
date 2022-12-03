a2345cd0dcadb1268e231e02d2c84141bd92e642
Chris_Vlachos <cv@mcinnovations.com.gr>
2022-12-03 16:06:55 +0200

    Styling with CSS modules - Scoped Styles

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -2,10 +2,12 @@
 
 import { Link } from "react-router-dom"
 
+import classes from "./MainNavigation.module.css"
+
 function MainNavigation() {
     return (
-        <header>
-        <div>React Meetups</div>
+    <header className={classes.header}>
+        <div className={classes.logo}>React Meetups</div>
         <nav>
             <ul>
                 <li>
@@ -21,37 +23,6 @@ function MainNavigation() {
         </nav>
     </header>
     )
-
-        // ----- NAVBAR USING BOOTSTRAP -------------
-
-        // < nav class="navbar navbar-expand-lg navbar-light bg-light" >
-        //     <div class="container-fluid">
-        //         <Link to='/' className="navbar-brand">React/BootStrap Meetups</Link>
-        //         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span class="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        //             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        //                 <li class="nav-item">
-        //                     <Link to='/new-meetup' className="nav-link active">
-        //                         Add New Meetups
-        //                     </Link>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <Link to='/' className="nav-link active">
-        //                         All Meetups
-        //                     </Link>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <Link to='/favorites' className="nav-link active">
-        //                         My Favorites
-        //                     </Link>
-        //                 </li>
-        //             </ul>
-
-        //         </div>
-        //     </div>
-        // </nav >
 }
 
 export default MainNavigation
