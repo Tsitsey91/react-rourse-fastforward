04c51fe4d7670f3b43f1cf3e057f097e944cb6bc
Chris_Vlachos <cv@mcinnovations.com.gr>
2022-12-03 16:42:51 +0200

    Ouputting Lists of Data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -3,9 +3,39 @@
  * will be loaded by our routing-package as pages, when 
  * a certain URL is typed 
  */
+const Dummy_Data = [
+    {
+        id: 'm1',
+        title: 'This is a first meetup',
+        image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Meetupstreet 5, 12345 Meetup City',
+        description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+    {
+        id: 'm2',
+        title: 'This is a second meetup',
+        image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Meetupstreet 5, 12345 Athens Yoooo',
+        description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+];
+
 
 function AllMeetupsPage() {
-    return <div>All Meetups Page</div>
+    return (
+        <div>
+            <h1>All Meetups Page</h1>
+            <ul>
+                {Dummy_Data.map(
+                    (meetup) => {return <li key={meetup.id}>{meetup.address}</li> }
+                )}
+            </ul>
+        </div>
+    )
 }
 
 export default AllMeetupsPage;
